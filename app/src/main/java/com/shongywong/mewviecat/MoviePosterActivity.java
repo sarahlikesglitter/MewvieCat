bccf11fa0119dcf52af1b72b2c8fd85c8c20528a
shongywong <shongywong@gmail.com>
2016-11-02 17:31:34 -0600

    added picasso to gradle

---
+++ app/src/main/java/com/shongywong/mewviecat/MoviePosterActivity.java
@@ -0,0 +1,30 @@
+package com.shongywong.mewviecat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class MoviePosterActivity extends Fragment
+{
+    public MoviePosterActivity()
+    {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState)
+    {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_movie_poster, container, false);
+    }
+
+}
